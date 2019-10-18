7935a0657bd2ad3203a27d24119f11a517fa5a3f
zvonimirpetrovic <zvonimir.petrovic10@gmail.com>
2019-10-18 15:17:12 +0200

    Added default route and routes for dashboard and hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,26 +8,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
 
-  getHeroes(): void{
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-  
-  selectedHero: Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-
-  constructor(private heroService: HeroService) { 
-  }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-    
     this.getHeroes();
-
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
