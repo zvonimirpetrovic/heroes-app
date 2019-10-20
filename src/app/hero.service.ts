d404814c7d10d661b18db26f87ea18ff3219d166
zvonimirpetrovic <zvonimir.petrovic10@gmail.com>
2019-10-20 15:04:03 +0200

    Implemented search functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -51,7 +51,7 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero (hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero) => this.log('added hero w/ if=${newHero.id}')),
+      tap((newHero: Hero) => this.log(`added hero w/ if=${newHero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
     )
   }
@@ -64,6 +64,18 @@ export class HeroService {
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /** GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      //if not search term, return an empty array
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log('found heroes matching "${term}"')),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 
