ea5d3c7536d4290cfa60f12a77894c63e1d0bdf0
zvonimirpetrovic <zvonimir.petrovic10@gmail.com>
2019-10-20 13:03:26 +0200

    Added error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -23,8 +24,33 @@ export class HeroService {
   
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
+  
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  
+ private handleError<T>(operation = 'operation', result?:T){
+   return (error: any): Observable<T> => {
+
+    //TODO: send the error to remote logging infrastructure
+    console.error(error);//log to console
+
+    //TODO: transforming error for user consumption
+    this.log('${operation} failer: ${error.message}');
+
+    // Keep app running by returning empty result
+    return of(result as T);
+   }
+ }
+
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
