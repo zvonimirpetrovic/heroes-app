30ad92915d19d1b05eb5ac6853d66f588bdc3f2d
zvonimirpetrovic <zvonimir.petrovic10@gmail.com>
2019-10-20 12:51:06 +0200

    Implemented HttpClientModule, inMemoryWebApi as mockup server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,18 +3,27 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient ) { }
   
+  private heroesUrl = 'api/heroes'; //Url to web api
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string){
+    this.messageService.add('HeroService: ${message}');
+  }
+  
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message after fetching heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of (HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
